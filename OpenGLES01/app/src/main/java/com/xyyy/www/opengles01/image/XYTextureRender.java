1ce01024bf2ea845602b4c2cc912624d68cffd75
liuml <liudao1@qq.com>
2018-12-04 13:50:20 +0800

    顶点缓冲对象 VBO

--- OpenGLES01/app/src/main/java/com/xyyy/www/opengles01/image/XYTextureRender.java
+++ OpenGLES01/app/src/main/java/com/xyyy/www/opengles01/image/XYTextureRender.java
@@ -31,15 +31,15 @@ public class XYTextureRender implements XYEGLSurfaceView.XYGLRender {
     private FloatBuffer vertexBuffer;
 
     private float[] fragmentData = {
-//            0f, 1f,
-//            1f, 1f,
-//            0f, 0f,
-//            1f, 0f
-
-            0f, 0.5f,
-            0.5f, 0.5f,
+            0f, 1f,
+            1f, 1f,
             0f, 0f,
-            0.5f, 0f
+            1f, 0f
+
+//            0f, 0.5f,
+//            0.5f, 0.5f,
+//            0f, 0f,
+//            0.5f, 0f
     };
     private FloatBuffer fragmentBuffer;
 
@@ -49,6 +49,7 @@ public class XYTextureRender implements XYEGLSurfaceView.XYGLRender {
     private int textureid;
     private int sampler;
 
+    private int vboId;
 
     public XYTextureRender(Context context) {
         this.context = context;
@@ -79,7 +80,25 @@ public class XYTextureRender implements XYEGLSurfaceView.XYGLRender {
         sampler = GLES20.glGetUniformLocation(program, "sTexture");
 
 
-        int []textureIds = new int[1];
+        int[] vbos = new int[1];
+//        1、创建VBO
+        GLES20.glGenBuffers(1, vbos, 0);
+        vboId = vbos[0];
+        //2.绑定VBO
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, vboId);
+        //3. 分配VBO需要的缓冲大小
+        GLES20.glBufferData(GLES20.GL_ARRAY_BUFFER, vertexData.length * 4, null, GLES20.GL_STATIC_DRAW);
+
+        //4,为VBO设置顶点数据的值(这里想象内存区域 先偏移0用于存储顶点坐标,再偏移顶点坐标的大小,用于存储片元坐标) 大小是顶点坐标加上片元坐标大小
+        GLES20.glBufferData(GLES20.GL_ARRAY_BUFFER, vertexData.length * 4 + fragmentData.length * 4, null, GLES20. GL_STATIC_DRAW);
+
+        GLES20.glBufferSubData(GLES20.GL_ARRAY_BUFFER, 0, vertexData.length * 4, vertexBuffer);
+        GLES20.glBufferSubData(GLES20.GL_ARRAY_BUFFER, vertexData.length * 4, fragmentData.length * 4, fragmentBuffer);
+//        5、解绑VBO
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, 0);
+
+
+        int[] textureIds = new int[1];
         GLES20.glGenTextures(1, textureIds, 0);
         textureid = textureIds[0];
 
@@ -111,21 +130,27 @@ public class XYTextureRender implements XYEGLSurfaceView.XYGLRender {
     public void onDrawFrame() {
 
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-        GLES20.glClearColor(1f,0f, 0f, 1f);
+        GLES20.glClearColor(1f, 0f, 0f, 1f);
 
         GLES20.glUseProgram(program);
 
         GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureid);
+
+        //绑定vbo
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, vboId);
+
         GLES20.glEnableVertexAttribArray(vPosition);
         GLES20.glVertexAttribPointer(vPosition, 2, GLES20.GL_FLOAT, false, 8,
-                vertexBuffer);
+                0);
 
         GLES20.glEnableVertexAttribArray(fPosition);
         GLES20.glVertexAttribPointer(fPosition, 2, GLES20.GL_FLOAT, false, 8,
-                fragmentBuffer);
+                vertexData.length * 4);
 
         GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
         GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
 
+        //解绑
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, 0);
     }
 }
